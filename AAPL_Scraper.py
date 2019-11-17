04f4e22460cae62c0baea8c2968986d47d5cef37
Damien Borowski <neimadb@hotmail.com>
2019-11-17 22:04:30 +0000

    Version 1.2
    Can now input any valid ticker to get price

--- AAPL_Scraper.py
+++ AAPL_Scraper.py
@@ -3,14 +3,41 @@ import smtplib
 import time
 import bs4
 import sys
+import PySimpleGUI as sg
 from bs4 import BeautifulSoup
 
-URL = 'https://www.marketwatch.com/investing/stock/aapl'
+site_to_scrape = "https://www.marketwatch.com/investing/stock/"
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
 
+def GUI():
+
+    global  site_to_scrape  
+
+    sg.change_look_and_feel('DarkAmber')	# Add a touch of color
+    # All the stuff inside your window.
+    layout = [[sg.Text('Enter ticker for real time data'), sg.InputText()],
+            [sg.Button('Search')] ]
+
+    # Create the Window
+    window = sg.Window('TradesMan', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, ticker = window.read()
+        if event in (None, 'Cancel'):	# if user closes window or clicks cancel
+            break
+        if len(ticker[0]) <= 4:
+            print('You entered ', ticker[0])
+            site_to_scrape += ticker[0]
+            print(site_to_scrape)
+
+    window.close()
+
 def check_price():
-    page = requests.get(URL,headers=headers)
+    try:
+        page = requests.get(site_to_scrape,headers=headers)
+    except:
+        print("Invalid Ticker")
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -21,8 +48,8 @@ def check_price():
     converted_price = float(price[4:10])
     print(converted_price)
 
-    if(converted_price > 200):
-        send_mail()
+    #if(converted_price > 200):
+        #send_mail()
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com',587)
@@ -47,6 +74,10 @@ def send_mail():
     server.quit
     sys.exit()
 
-while(True):
-    check_price()
-    time.sleep(60)
+#while(True):
+    #check_price()
+    #time.sleep(60)
+
+GUI()
+check_price()
+
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ version 1.1
   - Connects to Market watch for a specified ticker and retrieves real time data
   - Checks this realtime data until it reaches a specified target (Script checks it every 60 seconds(can change this))
   - If the price target is hit, sends me an email informing that the price target has been reached
+
+version 1.2
+  - Created basic GUI to get ticker from user
+  - Will now get price from any valid ticker
